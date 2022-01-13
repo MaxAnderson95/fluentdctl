9c0b72290c948c846e8dc58bfd5ac7f9af19901e
Max Anderson <cctpmax@gmail.com>
2022-01-13 15:39:43 -0500

    implemented verbose

--- fluentdctl/fluentdctl.py
+++ fluentdctl/fluentdctl.py
@@ -23,27 +23,34 @@ COMMAND_MAP = {
 
 def execute_command(host, port, command, verbose):
     command_text = COMMAND_MAP.get(command.value)
+    url = f"http://{host}:{port}/api/{command_text}"
+
+    if verbose:
+        print(
+            f"Performing command '{command_text}' against host '{host}' on port '{port}'. Raw URL: {url}")
+
     try:
-        r = requests.get(f"http://{host}:{port}/api/{command_text}")
-    except requests.exceptions.HTTPError as errh:
+        r = requests.get(url)
+    except requests.exceptions.HTTPError:
         return "Error: HTTP error occured"
-    except requests.exceptions.ConnectionError as errc:
+    except requests.exceptions.ConnectionError:
         return "Error: Target machine could not be reached or it actively refused the connection."
-    except requests.exceptions.Timeout as errt:
+    except requests.exceptions.Timeout:
         return "Error: Timeout error"
-    except requests.exceptions.RequestException as err:
+    except requests.exceptions.RequestException:
         return "Error: Unknown error occured"
 
     json = r.json()
     if json.get("ok") != True:
         return json
+    elif verbose:
+        return f"Command executed successfully. Raw output: '{json}'"
     else:
         pass
 
 
 def command_factory(command_type):
     def command(host: str = DEFAULT_HOST, port: int = DEFAULT_PORT, verbose: bool = False):
-        # return f"You've performed the {command_type.name} command against {host}:{port}"
         return execute_command(host, port, command_type, verbose)
     return command
 
